68186ef893f684b78eb976e751d370024caf14ec
Tom Kim <ekagud@gmail.com>
2018-11-10 17:31:06 +0900

    elastic-beanstalk 브랜치 추가

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('AWS exercise의 A project입니다.');
+  res.send('Elastic Beanstalk으로 배포된 AWS exercise의 A project입니다.');
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
 });
 
 app.get('/health', (req, res) => {
