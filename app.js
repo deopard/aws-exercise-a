c26db82ba3da86bad022649936f2dac04fe696c1
Tom Kim <ekagud@gmail.com>
2018-11-10 17:19:04 +0900

    연습용 기본 코드 추가

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('AWS exercise의 A project입니다.');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send();
+});
